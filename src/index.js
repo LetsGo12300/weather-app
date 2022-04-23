0bc9956cdd595bbad70777039a7a4e87ccd034fb
Kat <katkatvdd@yahoo.com.ph>
2022-04-23 23:48:08 +0800

    Added code to display error message when API returns 404 (not found)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 // DOM controller
-const info = document.querySelector('.info');
+const container = document.querySelector('.container');
 const cityName = document.querySelector('.city-name');
 const mainTemperature = document.querySelector('.temp');
 const humidity = document.querySelector('.humidity');
@@ -9,9 +9,11 @@ const description = document.querySelector('.description');
 const minTemperature = document.querySelector('#min-temp');
 const maxTemperature = document.querySelector('#max-temp');
 const windSpeed = document.querySelector('.wind-speed');
+const errorDiv = document.querySelector('.error-div');
 
 const displayController = (() => {
     const updateInfo = (city, country, mainTemp, minTemp, maxTemp, hum, wSpeed, desc) => {
+        clearErrorDiv();
         cityName.textContent = `${city}, ${country}`;
         mainTemperature.textContent = `${mainTemp}°C`;
         humidity.textContent = `Humidity: ${hum}%`;
@@ -20,17 +22,28 @@ const displayController = (() => {
         minTemperature.textContent = `Min. temp: ${minTemp}°C`;
         maxTemperature.textContent = `Max. temp: ${maxTemp}°C`;
     };
+    const showError = (error) => {
+        errorDiv.textContent = error.message;
+    };
+    const clearErrorDiv = () => {
+        errorDiv.textContent = '';
+    };
     return {
-        updateInfo
+        updateInfo,
+        showError
     }
 })();
 
 // API 
 function getWeather(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=00d952e95b26f8bd03c973b670e05f6c&units=metric`, {mode: 'cors'})
-    .then(response => response.json())
-    .then(data => { 
-        console.log(data);
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+          }
+        throw new Error('No matching location found!');
+    })
+    .then(data => {
         const city = data.name;
         const country = data.sys.country;
         const mainTemp = data.main.temp;
@@ -41,7 +54,9 @@ function getWeather(city){
         const description = data.weather[0].description;
         displayController.updateInfo(city, country, mainTemp, minTemp, maxTemp, humidity, windSpeed, description);
     })
-    .catch(error => console.log(error))
+    .catch(error => {
+        displayController.showError(error);
+    })
 }
 
 // Event listeners
@@ -53,3 +68,4 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     getWeather(formInput);
 });
+
